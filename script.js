d66325a2c5fa86bf5a88eec5339b80365674675f
saddamarbaa <saddamarbaas@gmail.com>
2020-12-31 01:12:14 +0700

    Rest Parameter

--- script.js
+++ script.js
@@ -175,3 +175,26 @@ function showMyInfo(userName = "saddam", role = "Admin", them = "black") {
 console.log(showMyInfo("Saddam", "IT", "Them"));
 console.log(showMyInfo());
 console.log(showMyInfo("Saddam", "Them"));
+
+// [Rest Parameter]
+
+function demoTest(param1, param2, param3, ...myParam) {
+  console.log("param1 : ", param1);
+  console.log("param2 : ", param2);
+  console.log("param3 : ", param3);
+  console.log("Rest of Parameter : ", myParam);
+}
+
+demoTest("A", "B", "C", "ABCDEF");
+
+function addAllParams(...myParams) {
+  let myNumber = 0;
+  for (const param of myParams) {
+    myNumber += param;
+  }
+  return myNumber;
+}
+
+console.log(addAllParams(1)); /* Output: 1 */
+console.log(addAllParams(1, 2, 3, 4)); /* Output: 10 */
+console.log(addAllParams(5, 5, 5, 5, 5)); /* Output: 25 */

0f5846a29dbe705c01f0898eb3b95be456d1728e
saddamarbaa <saddamarbaas@gmail.com>
2020-12-30 15:48:13 +0700

    lecture 2 New Features in ES6

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+/*
+			[lecture 01 Introduction ES6]
+			
+   ES6, ES7, ES8
+			[E]CMAS [c] ript 6  / ECMAScript 2015 - ES6
 
+*/
+
+/* 
+			[lecture 2 New Features in ES6]
+			
+    The let keyword
+    The const keyword
+    JavaScript Arrow Functions
+    JavaScript Class
+    JavaScript Promise
+    JavaScript Symbol
+    Default Parameter Values
+    Function Rest Parameter
+    Array.find()
+    Array.findIndex()
+    New Number Properties
+    New Number Methods
+    New Global Methods
+
+			[var]
+			- Function Sope
+			- can Be ReDeclare
+			- Undefined When Accessing Variable Before its Declared
+			- Does Not Create Properties in the Windows Object
+
+			[let]
+			- Block Sope
+			- cannot Be ReDeclare
+			- ReferenceEror When Accessing Variable Before its Declared
+			- Does Not Create Properties in the Windows Object
+
+			[const]
+			- Block Sope
+			- cannot Be ReDeclare
+			- ReferenceEror When Accessing Variable Before its Declared
+			- Does Not Create Properties in the Windows Object
+*/

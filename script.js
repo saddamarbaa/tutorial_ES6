d0a898b31e2195f8764df7342802b92b6906928a
saddamarbaa <saddamarbaas@gmail.com>
2020-12-30 21:16:33 +0700

    added Template Literals

--- script.js
+++ script.js
@@ -121,3 +121,17 @@ console.log(arrawTestFour(10, 5));
 /*
 			[lArrow function and this keyword in javascript]
 	*/
+
+// [Template Literals]
+
+let fullName = "Jhon Do";
+
+function makeUpperCase(word) {
+  return word.toUpperCase();
+}
+
+let template = `<h1> ${makeUpperCase("Hello")}  ${fullName}</h1> 
+		<p>this is template literals from javascript</p>
+		<h4>thanks for comming </h4>`;
+
+document.getElementById("template").innerHTML = template;

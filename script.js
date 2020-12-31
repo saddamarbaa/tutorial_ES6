25d8821a7cd0be7ba3dfd15be21b1b67f82acbfc
saddamarbaa <saddamarbaas@gmail.com>
2021-01-01 06:44:37 +0700

    Set vs Array

--- script.js
+++ script.js
@@ -378,3 +378,20 @@ function showStudentInfo({
 }
 
 showStudentInfo(student);
+
+/*  Set vs Array
+    set: new Set(Iterable)
+*/
+
+let mySet = new Set([1, 2, 1, 1, 1, 3, 3, 3, 4, 3, 4]);
+let myArray = [1, 2, 1, 1, 1, 1, 3, 4];
+
+console.log(typeof mySet); /* object*/
+console.log(myArray); /* output: [1, 2, 1, 1, 1, 1, 3, 4] */
+console.log(mySet); /* output:{1, 2, 3, 4} */
+
+//convert an array to set
+let newSet = new Set(myArray);
+// convert  set to an array
+let newArray = [...mySet];
+console.log(newSet);

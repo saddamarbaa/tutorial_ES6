a4b48857a0f13b6d771914f7828642fc426482a3
saddamarbaa <saddamarbaas@gmail.com>
2021-01-01 09:16:31 +0700

    forEach With Set And Map

--- script.js
+++ script.js
@@ -464,3 +464,27 @@ testDemo.forEach((element, indext) => {
   // console.log(element);
   console.log(`${indext} : ${element}`);
 });
+
+// forEach With Set
+
+const demoSet = new Set([1, 1, 1, 2, 2, 3, 3, 4, 4, 12, 2, 3]);
+console.log(demoTest);
+
+demoSet.forEach((item, item2) => {
+  console.log(item);
+  // console.log(`${item},${item2}`);
+});
+
+// forEach With Map
+
+const demoMap = new Map([
+  ["A", "sadam"],
+  ["B", 2],
+  ["x", { nam: "adam", age: 23, gender: "male" }],
+]);
+
+// console.log(demoMap);
+
+demoMap.forEach((value, key) => {
+  console.log(`${key} ==>${value}`);
+});

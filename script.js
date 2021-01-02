3d5caff0b4c2ad285e54ab3e71e5cd195cce8b41
saddamarbaa <saddamarbaas@gmail.com>
2021-01-02 14:04:08 +0700

    Symbol Iterator And For Of Loop

--- script.js
+++ script.js
@@ -602,8 +602,8 @@ console.log(mainTestingMap.get(thirdObject));
 						- Object With Interface Designed For Iteration
 						- All Iterators Has Next Method That Return Result Object
 						- The Result Object Has Two Props
-						-- [1] Value =>The Next Value
-						--[2] Done => Boolean Value To tell If The Next Value Exists or No
+						-- [1] Value => The Next Value
+						-- [2] Done => Boolean Value To tell If The Next Value Exists or No
 
 						For  ... Of Loop 
 
@@ -611,3 +611,46 @@ console.log(mainTestingMap.get(thirdObject));
 						[2] Save Value Inside variable
 						[3] Check For Done If Its True => Exit The Loop
 		*/
+
+let testName = "sadam";
+let testNumber = 1223;
+let testArray = [1, 2, 3, 4, 5];
+let testObject = {};
+
+console.log(typeof testName[Symbol.iterator]); // out : function
+console.log(typeof testArray[Symbol.iterator]); // out : function
+console.log(typeof testNumber[Symbol.iterator]); // out : undefind
+console.log(typeof testObject[Symbol.iterator]); // out :  undefind
+
+let TheIterator = testArray[Symbol.iterator]();
+console.log(TheIterator);
+console.log(TheIterator.next());
+console.log(TheIterator.next());
+console.log(TheIterator.next());
+console.log(TheIterator.next());
+console.log(TheIterator.next());
+console.log(TheIterator.next());
+
+let TheStringIterator = testName[Symbol.iterator]();
+console.log(TheStringIterator.next());
+console.log(TheStringIterator.next());
+console.log(TheStringIterator.next());
+console.log(TheStringIterator.next());
+console.log(TheStringIterator.next());
+console.log(TheStringIterator.next());
+
+// console.log(typeof testNumber[Symbol.iterator]());
+// console.log(typeof testObject[Symbol.iterator]())
+
+for (const char of testName) {
+  console.log(char);
+}
+
+// TypeError: testNumber is not iterable
+// for (const num of testNumber) {
+//   console.log(num);
+// }
+
+for (const iterator of testArray) {
+  console.log(iterator);
+}

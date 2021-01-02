e73a84fa7d03a3e48cc4899dace4bad623a8b984
saddamarbaa <saddamarbaas@gmail.com>
2021-01-03 06:35:18 +0700

    Modules - Part  Import & Export

---
+++ app.js
@@ -0,0 +1,18 @@
+export const userAge = 23;
+// export an array
+export let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+// export function
+export function sayHello(userName) {
+  return `Hello ${userName} nice to see You`;
+}

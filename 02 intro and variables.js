5fba6b997e9234d16f60c81258741e1c9b6a24e5
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2021-01-03 15:38:02 +0700

    "Introduction To ES6, ES7, ES8 and Variable"

--- 02 intro and variables.js
+++ 02 intro and variables.js
@@ -2,7 +2,7 @@
    [Introduction ES6]
 			
    ES6, ES7, ES8
-   [E]CMAS [c] ript 6  / ECMAScript 2015 - ES6
+   [E]CMAS [c]cript 6  / ECMAScript 2015 - ES6
 
     [New Features in ES6]
 			
@@ -20,23 +20,23 @@
     New Number Methods
     New Global Methods
 
-	[var]
-	 - Function Scope
+  [var]
+  - Function Scope
   - Can Be Redeclare
   - Undefined When Accessing A Variable Before It's Declared
-		- Create Properties in The Window Object
+ - Create Properties in The Window Object
 		
-	[The let keyword]
+ [The let keyword]
  - Block Scope
   - Cannot Be Redeclare
   - Reference Error When Accessing A Variable Before It's Declared
   - Does Not Create Properties in The Window Object
 
-	[The const keyword]
-		- Block Scope
-	- Cannot Be Redeclare
-	- Reference Error When Accessing A Variable Before It's Declared
-	- Does Not Create Properties in The Window Object
+  [The const keyword]
+  - Block Scope
+  - Cannot Be Redeclare
+  - Reference Error When Accessing A Variable Before It's Declared
+  - Does Not Create Properties in The Window Object
 */
 
 const PI = 3.14;

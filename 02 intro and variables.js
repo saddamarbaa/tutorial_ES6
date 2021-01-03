8dd754ca27a70aa41632c8db7fb1fd3741fb1182
saddamarbaa <saddamarbaas@gmail.com>
2021-01-03 15:34:31 +0700

    Introduction to ES6, ES7, ES8 and Variable

---
+++ 02 intro and variables.js
@@ -0,0 +1,45 @@
+/*
+   [Introduction ES6]
+			
+   ES6, ES7, ES8
+   [E]CMAS [c] ript 6  / ECMAScript 2015 - ES6
+
+    [New Features in ES6]
+			
+    The let keyword
+    The const keyword
+    JavaScript Arrow Functions
+    JavaScript Class
+    JavaScript Promise
+    JavaScript Symbol
+    Default Parameter Values
+    Function Rest Parameter
+    Array.find()
+    Array.findIndex()
+    New Number Properties
+    New Number Methods
+    New Global Methods
+
+	[var]
+	 - Function Scope
+  - Can Be Redeclare
+  - Undefined When Accessing A Variable Before It's Declared
+		- Create Properties in The Window Object
+		
+	[The let keyword]
+ - Block Scope
+  - Cannot Be Redeclare
+  - Reference Error When Accessing A Variable Before It's Declared
+  - Does Not Create Properties in The Window Object
+
+	[The const keyword]
+		- Block Scope
+	- Cannot Be Redeclare
+	- Reference Error When Accessing A Variable Before It's Declared
+	- Does Not Create Properties in The Window Object
+*/
+
+const PI = 3.14;
+const name = "Saddam";
+console.log(PI);
+console.log(name);

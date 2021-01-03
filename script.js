a708549ef2cac023d219bf806d35fcc23f5e61ff
saddamarbaa <saddamarbaas@gmail.com>
2021-01-03 13:36:50 +0700

    Promise Intro(update code)

--- script.js
+++ script.js
@@ -959,14 +959,26 @@ let stud3 = new studentMemember("Jhon Joe", 19, 21, 3.4);
 stud3.getGpa();
 stud3.register();
 
-// Call Stack, Web API, Event Loop, Callback Queue
-
-/*
-		out for blow will be
-			first
-			thrid
-			second
-			last excuit
+/* 
+ Call Stack, Web API, Event Loop, Callback Queue
+  Important Knowledge
+  For Promises
+  - Call Stack
+  --- Mechanism To Make Interpreter Track Your Calls
+  --- When You Call A Function Its Added To The Stack
+  --- When Function Executed Its Removed From The Stack
+  --- The Interpreter Continue Calling From The Last Point Reached
+  --- Call Stack Detect Web API And Leave It To The Browser To Handle It
+  - Web API
+  --- Methods Available From Environment => Browser
+  --- When Complete It Add The Callback To The Callback Queue
+  - Event Loop
+  --- Wait The Call Stack To Finish
+  --- Get Callback From Callback Queue
+  --- Add Callback To Call Stack
+  - Callback Queue
+  =============================================
+  Asynchronous Vs Synchronous Programming
 */
 
 window.setTimeout(() => console.log("last excuit"), 10);
@@ -985,3 +997,36 @@ console.log("Function To Do Something");
 window.setTimeout(() => console.log("Get Friend List", 1000));
 window.setTimeout(() => console.log("Get lates feed back", 2000));
 console.log("Do Important Things,Waiting is A big Problem");
+
+/*
+  Promise Intro And Discussions
+  Promise Intro
+  - Promise Is One Of The Best Features In ES6
+  - Promise Make It Easy To Support Asynchronous Programming
+  - Promise in JavaScript Is Like Promise in Real Life
+  - Promise Is Something That Will Happen in The Future
+  - Promise Is A Placeholder For A Future Value
+  - You Booked A Cinema Ticket, You Have A Promise From Cinema Owner 2 Have A Seat
+  - You Got Task To Do, U Promised Your Boss To Do The Task
+  - You Ordered Food From Restaurant
+  --- [ Promise ] Is The Order Record
+  --- [ The Future Value ] Is The Food You Will Receive
+  - You Ordered iPhone From Souq Shop
+  --- [ Promise ] Is The Order Record
+  --- [ The Future Value ] Is The iPhone You Will Receive
+  - Promise Is An Object
+  - Promise Is The Event Completion Or Failure of Async Operation & Its Resulting Value.
+  - JavaScript Use Callbacks For Asynchronous Programming
+  --- [1] Function Called And Do The Task
+  --- [2] Action Complete
+  --- [3] Another Function Called
+  --- [4] Action Complete
+  --- [5] Another Function Called
+  - In Complex Cases => Every Call Add Nesting Level
+  - http://callbackhell.com/
+  - Promise Avoiding Callback Hell Or Pyramid Of Doom
+  - Promise Is One Of This States
+  [1] Pending => Not Fulfilled Or Rejected
+  [2] Fulfilled => Operation Succeeded
+  [3] Rejected => Operation Failed
+*/

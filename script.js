7bbec3587c9ede22a8be6ae46ff959893519235d
saddamarbaa <saddamarbaas@gmail.com>
2021-01-03 13:17:59 +0700

    Asynchronous vs Synchronous Programming

--- script.js
+++ script.js
@@ -978,3 +978,10 @@ console.log("thrid");
 window.setTimeout(() => console.log(numberForTest), 0);
 let numberForTest = 12; // excuit first
 numberForTest += 1; // excuit second
+
+// Asynchronous vs Synchronous Programming
+
+console.log("Function To Do Something");
+window.setTimeout(() => console.log("Get Friend List", 1000));
+window.setTimeout(() => console.log("Get lates feed back", 2000));
+console.log("Do Important Things,Waiting is A big Problem");
